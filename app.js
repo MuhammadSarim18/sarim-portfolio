efb8d2ff93c1c4caec36e1ba05449f1fcb052997
Muhammad Sarim <muhammadsarimofficial17@gmail.com>
2026-01-04 22:42:48 +0500

    upload recent projects

--- app.js
+++ app.js
@@ -1,58 +1,6 @@
-const tl = gsap.timeline();
-
-
-tl.to("body", {
-  overflow: "hidden",
-
-})
-  .to(".preloader .text-container", {
-    duration: 0,
-    opacity: 1,
-
-    ease: "Power3.easeOut"
-  })
-  .from(".preloader .text-container h1", {
-    duration: 1.5,
-    delay: 0.2,
-    y: 200,
-    skewY: 10,
-    stagger: 0.4,
-
-    ease: "Power3.easeOut"
-  })
-  .to(".preloader .text-container h1", {
-    duration: 1.2,
-    y: 200,
-    skewY: -20,
-    stagger: 0.2,
-
-    ease: "Power3.easeOut"
-  })
-  .to(".preloader", {
-    duration: 1,
-
-
-    height: "0vh",
-    ease: "Power3.easeOut"
-  })
-  .to(
-    "body",
-    {
-      overflow: "auto"
-    },
-    "-=1"
-  )
-  .to(".preloader", {
-    display: "none",
-  });
 
 
 
-function menuClick(){
-  var menu_bar = document.getElementById('menu_bar');
-  var nav_links = document.getElementsByClassName('nav-links');
-  console.log(nav_links);
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   const menuIcon = document.querySelector('.menu-icon');
